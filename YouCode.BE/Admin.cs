48d8def3794656d4ddb6ead01b4d35f58f4c314e
Katherin <katherinenonasco@gmail.com>
2024-02-19 22:17:04 -0600

    entidades de la capa BE

---
+++ YouCode.BE/Admin.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Reflection.Metadata;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace YouCode.BE
+{
+    public class Admin
+    {
+        [Key]
+        public int Id { get; set; }
+        [ForeignKey("User")]
+        public int IdUser { get; set; }
+        public DateTime CreatedAt { get; set; }
+
+        [NotMapped]
+        public User User = new User();
+    }
+}
