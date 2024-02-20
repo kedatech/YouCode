5dc740d5fd31f3ede97b078fc83e2dc9c60ee922
Gabriela Herrera <gaby-herrera-dev@outlook.es>
2024-02-19 21:43:38 -0600

    fix: conexion a la base de datos

--- YouCode.DAL/ContextoDB.cs
+++ YouCode.DAL/ContextoDB.cs
@@ -1,12 +1,36 @@
-﻿using System;
+﻿using Microsoft.EntityFrameworkCore;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using YouCode.BE;
 
 namespace YouCode.DAL
 {
-    internal class ContextoDB
+    public class ContextoDB : DbContext
     {
+        public DbSet<User> User { get; set; }
+        public DbSet<Post> Post { get; set; }
+        public DbSet<Comment> Comment { get; set; }
+        public DbSet<Image> Image { get; set; }
+        public DbSet<Video> Video { get; set; }
+        public DbSet<Group> Group { get; set; }
+        public DbSet<Report> Report { get; set; }
+        public DbSet<Favorite> Favorite { get; set; }
+        public DbSet<Admin> Admin { get; set; }
+        public DbSet<Follower> Follower {  get; set; }
+        public DbSet<Reaction> Reaction { get ; set; }
+        public DbSet<Config> Config { get; set; }
+        public DbSet<Profile> Profile { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer(@"Data source = DESKTOP-PBLCGGH;
+                                        Initial Catalog = YouCode;
+                                        Integrated Security = true;
+                                        Encrypt = false;
+                                        trustServerCertificate = true");
+        }
     }
 }
--- YouCode.DAL/UserDAL.cs
+++ YouCode.DAL/UserDAL.cs
@@ -3,10 +3,21 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using YouCode.BE;
 
 namespace YouCode.DAL
 {
     internal class UserDAL
     {
+        public static async Task<int> CreateAsync(User user)
+        {
+            int result = 0;
+            using (var bdContexto = new ContextoDB())
+            {
+                bdContexto.Add(user);
+                result = await bdContexto.SaveChangesAsync();
+            }
+            return result;
+        }
     }
 }
